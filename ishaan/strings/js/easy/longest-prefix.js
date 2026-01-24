aaa577187b256519783ec1b1b1aabe5f7cb0263e
Ishaan Barde <ishaanbarde9@gmail.com>
2026-01-24 23:38:30 +0530

    added comments in leetcode qts 14

--- ishaan/strings/js/easy/longest-prefix.js
+++ ishaan/strings/js/easy/longest-prefix.js
@@ -1,8 +1,13 @@
+// Leetcode Qts: 14. Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+
 const strs = ["flower", "flow", "flight"];
 
 function longestCommonPrefix(strs) {
   if (strs.length === 0) return;
 
+  // Sorting lexographically
   strs.sort();
 
   const first = strs[0];
