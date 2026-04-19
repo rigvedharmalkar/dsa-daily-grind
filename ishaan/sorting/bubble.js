35598fc30c91db18ffe89d8f7941d5561ebb1f91
Ishaan Barde <ishaanbarde9@gmail.com>
2026-04-19 17:43:54 +0530

    bubble sort solution

--- ishaan/sorting/bubble.js
+++ ishaan/sorting/bubble.js
@@ -2,14 +2,17 @@
 
 const arr = [98, 64, 1, 76, 11];
 
-for (let i = 0; i < arr.length - 1; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[i] > arr[j]) {
-      let temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+function bubbleSort(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+      }
     }
   }
+  return nums.length ? nums : -1;
 }
 
-console.log(arr);
+console.log(bubbleSort(arr));
